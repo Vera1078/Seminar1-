ab82a579eb554bc022dbe9960918d78c66608e22
Vera Lippgardt <vera1078@mail.ru>
2022-08-19 20:48:56 +0300

    Задание на определение max числа из  чиселa, b

--- Aufgabe2/Program.cs
+++ Aufgabe2/Program.cs
@@ -1,15 +1,15 @@
-﻿Console.WriteLine("Введите первое число ");
+﻿Console.Write("a = ");
 int number1 = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Введите второе число ");
+System.Console.Write("b = ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 int maxnumber = number1;
 if (number2 > maxnumber)
 {
   maxnumber = number2;
-  System.Console.WriteLine("Максимальное число: " + maxnumber);  
+  System.Console.WriteLine("max = " + maxnumber);  
 }
 else
 {
    maxnumber = number1; 
-System.Console.WriteLine("Максимальное число: " + maxnumber);
+System.Console.WriteLine("max = " + maxnumber);
 }
