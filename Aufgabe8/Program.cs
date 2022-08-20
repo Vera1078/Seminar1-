5e4fcaa119e233fd0e39c0ecd6d93914c188e6ed
Vera Lippgardt <vera1078@mail.ru>
2022-08-20 13:50:25 +0300

    Четное число от 1 до N

---
+++ Aufgabe8/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.Write("Введите значение n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+
+while (i <= n)
+{
+    if (i % 2 == 0)
+
+    {
+     System.Console.Write (i + " ");
+       
+    }
+    i ++;
+    }
+            
+
+
