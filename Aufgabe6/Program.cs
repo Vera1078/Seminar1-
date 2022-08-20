f5a4b7cbac2a20ac1ff13b5a91577a7b0d5af6b0
Vera Lippgardt <vera1078@mail.ru>
2022-08-20 11:27:30 +0300

    Поиск четного числа

---
+++ Aufgabe6/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    System.Console.WriteLine("да");
+    }
+    else
+    {
+        System.Console.WriteLine("нет");
+    }
